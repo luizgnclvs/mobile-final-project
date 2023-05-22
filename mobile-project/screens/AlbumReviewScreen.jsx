9e7f478e637f993ea2a668476d750b2c5abb1f35
Mariana <bulhoesmari@gmail.com>
2023-05-22 12:30:42 -0300

    add zustand AlbumReviewScreen

--- mobile-project/screens/AlbumReviewScreen.jsx
+++ mobile-project/screens/AlbumReviewScreen.jsx
@@ -1,69 +1,73 @@
-import * as React from 'react';
+import React from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import useStore from '../store/store';
 
 export default function AlbumReviewScreen({ route, navigation }) {
-	const { album } = route.params;
+  const { album } = route.params;
+  const addReview = useStore((state) => state.addReview);
 
-	const [comment, setComment] = React.useState('');
-	const [rating, setRating] = React.useState(0);
+  const [comment, setComment] = React.useState('');
+  const [rating, setRating] = React.useState('');
 
-	function handleCommentChange(value) {
-		setComment(value);
-	}
+  const handleCommentChange = (value) => {
+    setComment(value);
+  };
 
-	function handleRatingChange(value) {
-		if (!isNaN(value)) setRating(Number(value));
-	}
+  const handleRatingChange = (value) => {
+    if (!isNaN(value)) {
+      setRating(Number(value));
+    }
+  };
 
-	function handleSubmit() {
-		// Aqui você pode enviar a avaliação do usuário para o servidor ou para o banco de dados local.
-		// Você pode usar bibliotecas como axios ou fetch para isso.
-	}
+  const handleSubmit = () => {
+    addReview(album.id, comment, rating);
+    navigation.goBack();
+  };
 
-	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>{album.name}</Text>
-			<Text style={styles.subtitle}>{album.artist}</Text>
-			<TextInput
-				style={styles.input}
-				placeholder="Comentário"
-				onChangeText={handleCommentChange}
-				value={comment}
-			/>
-			<TextInput
-				style={styles.input}
-				placeholder="Avaliação (0-5)"
-				keyboardType="numeric"
-				onChangeText={handleRatingChange}
-				value={String(rating)}
-			/>
-			<Button title="Enviar" onPress={handleSubmit} />
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{album.name}</Text>
+      <Text style={styles.subtitle}>{album.artist}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Comentário"
+        onChangeText={handleCommentChange}
+        value={comment}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Avaliação (0-5)"
+        keyboardType="numeric"
+        onChangeText={handleRatingChange}
+        value={String(rating)}
+      />
+      <Button title="Enviar" onPress={handleSubmit} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		padding: 10,
-		backgroundColor: '#fff',
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	title: {
-		fontSize: 24,
-		fontWeight: 'bold',
-		marginBottom: 10,
-	},
-	subtitle: {
-		fontSize: 18,
-		marginBottom: 20,
-	},
-	input: {
-		borderWidth: 1,
-		borderRadius: 5,
-		padding: 10,
-		marginBottom: 10,
-		width: '100%',
-	},
+  container: {
+    padding: 10,
+    backgroundColor: '#fff',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    width: '100%',
+  },
 });
