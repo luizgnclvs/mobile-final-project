ca54944e980d097bb2ac6fea6bc9e577e5eed6fb
luizgnclvs <luizgnclvss@gmail.com>
2023-06-10 22:57:28 -0300

    Add método post ao album service

--- mobile-project/services/album.service.js
+++ mobile-project/services/album.service.js
@@ -1,3 +1,5 @@
 import axiosInstance from "./api.instance";
 
 export const getAlbums = () => axiosInstance.get("albums").then(res => res.data);
+
+export const createAlbum = newAlbum => axiosInstance.post("albums", newAlbum).then(res => res.data);
