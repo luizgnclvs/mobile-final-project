28df37ee236016eceed44abc9cdc853eafeefd6a
luizgnclvs <luizgnclvss@gmail.com>
2023-06-10 22:58:52 -0300

    Impl. albumFormStore em AlbumCreateScreen

--- mobile-project/screens/AlbumCreateScreen.jsx
+++ mobile-project/screens/AlbumCreateScreen.jsx
@@ -2,26 +2,41 @@ import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Button, TextInput, View } from 'react-native';
 import { Text } from 'react-native-paper';
+import { createAlbum } from '../services/album.service';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import useAlbumFormStore from '../stores/album-form.store';
 
 export default function AlbumCreateScreen({ route, navigation }) {
-	const [name, setName] = useState('');
-	const [artist, setArtist] = useState('');
-	const [coverUrl, setCoverUrl] = useState('');
+	const queryClient = useQueryClient();
 
-	const handleCreateAlbum = () => {
-		const albumData = {
+	const {
+		name,
+		artist,
+		coverURL,
+		releaseYear,
+		setName,
+		setArtist,
+		setCoverURL,
+		setReleaseYear,
+	} = useAlbumFormStore();
+
+	const submitNewALbum = () => {
+		const albumForm = {
 			name: name,
 			artist: artist,
+			cover_url: coverURL,
+			release_year: releaseYear,
 		};
 
-		axiosInstance.post('/albums', albumData)
-			.then(response => {
-				console.log(response.data);
-			})
-			.catch(error => {
-				console.error(error);
-			});
+		createAlbum(albumForm);
 	};
+
+	const mutation = useMutation({
+		mutationFn: submitNewALbum,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['albums'] });
+		},
+	});
 
 	return (
 		<View style={styles.container}>
@@ -29,28 +44,32 @@ export default function AlbumCreateScreen({ route, navigation }) {
 			<TextInput
 				label="Nome do Álbum"
 				value={name}
-				onChangeText={setName}
+				onChangeText={value => setName(value)}
 				style={styles.input}
 				placeholder="Nome do Álbum"
-
 			/>
 			<TextInput
 				label="Artista"
 				value={artist}
-				onChangeText={setArtist}
+				onChangeText={value => setArtist(value)}
 				style={styles.input}
 				placeholder="Artista"
-
 			/>
 			<TextInput
 				label="URL da Capa"
-				value={coverUrl}
-				onChangeText={setCoverUrl}
+				value={coverURL}
+				onChangeText={value => setCoverURL(value)}
 				style={styles.input}
 				placeholder="URL da Capa"
-
 			/>
-			<Button title="Criar Álbum" onPress={handleCreateAlbum} />
+			<TextInput
+				label="Ano de Lançamento"
+				value={releaseYear}
+				onChangeText={value => setReleaseYear(value)}
+				style={styles.input}
+				placeholder="Ano de Lançamento"
+			/>
+			<Button title="Criar Álbum" onPress={mutation.mutate} />
 		</View>
 	);
 }
