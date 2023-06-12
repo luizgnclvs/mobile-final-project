2f768daf8b1cc29c9a556546a71fdcbb784ba531
luizgnclvs <luizgnclvss@gmail.com>
2023-06-12 01:27:33 -0300

    Services

--- mobile-project/services/album.service.js
+++ mobile-project/services/album.service.js
@@ -1,6 +1,8 @@
 import axiosInstance from "./api.instance";
 
-export const getAlbums = async () => axiosInstance.get("albums").then(res => albums = res.data);
+export const getAlbums = async () => axiosInstance.get("albums").then(res => res.data);
+
+export const getAlbumById = async (id) => axiosInstance.get(`albums?id=${id}`).then(res => res.data);
 
 export const createAlbum = async newAlbum =>
 	axiosInstance.post("albums", newAlbum)
